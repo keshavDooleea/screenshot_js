240a02979610412572843fc841f6852ed6baa1aa
keshav <kdooleea@yahoo.ca>
2021-03-28 23:24:45 -0400

    added possibility to open screenshot folder in command with -o

--- screenshot.js
+++ screenshot.js
@@ -3,32 +3,41 @@ const puppeteer = require("puppeteer");
 const cmd = require("node-cmd");
 
 /**
- * node screenshot.js URL FILENAME delay
+ * node screenshot.js URL FILENAME delay -o
  * delay is in seconds and is optional
+ * -o to open the folder after taking screenshot
  */
+const OPEN_SCREENSHOT_FOLDER_COMMAND = "-o";
+const DEFAULT_DELAY = 10000; // 10 seconds
 
 const URL = process.argv[2];
 const FILE_NAME = process.argv[3];
 const SCREENSHOT_FOLDER_PATH = process.env.SCREENSHOT_PATH;
 const PATH = `${SCREENSHOT_FOLDER_PATH}/${FILE_NAME}.png`;
 
-// open screenshots folder
-if (process.argv.length === 3 && URL === "o") {
+// launch file explorer by opening screenshots folder
+const openScreenshotFolder = () => {
   cmd.runSync(`start ${SCREENSHOT_FOLDER_PATH}`);
   console.log("Opened screenshot folder");
   process.exit(1);
+};
+
+// open screenshots folder: node screenshot.js o
+if (process.argv.length === 3 && URL === OPEN_SCREENSHOT_FOLDER_COMMAND) {
+  openScreenshotFolder();
 }
 
 // no args entered
 if (!URL || !FILE_NAME) {
   let missedArgs = !URL ? "Url " : "";
   missedArgs += !FILE_NAME ? "FileName" : "";
-  console.log(`Missing arguments: ${missedArgs} (Delay)`);
+  console.log(`Missing arguments: ${missedArgs} (Delay) (o)`);
   process.exit(1);
 }
 
 // delay used to wait for images to load
-const DELAY = process.argv[4] ? process.argv[4] * 1000 : 10000;
+// if there's a delay arg, convert to seconds! else, delay = default
+const DELAY = process.argv[4] && process.argv[4] !== OPEN_SCREENSHOT_FOLDER_COMMAND ? process.argv[4] * 1000 : DEFAULT_DELAY;
 
 // check if all images on website have loaded
 const imagesHaveLoaded = () => {
@@ -79,5 +88,5 @@ const timeout = async (ms) => {
 
   console.log("5/5 Done");
   await browser.close();
-  process.exit(1);
+  process.argv.includes(OPEN_SCREENSHOT_FOLDER_COMMAND) ? openScreenshotFolder() : process.exit(1);
 })();
